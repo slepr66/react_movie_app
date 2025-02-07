8997c35b2a70ca434a01b499e1b223f7935c3ff8
slepr66 <shinaniyash@gamil.com>
2025-02-07 17:51:20 +0500

    added some components and created routing for pages

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites() {
+
+    return (
+        <div className="favourites">
+            <h2>No Favourites yet!</h2>
+        </div>
+    );
+}
+
+export default Favourites
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "Jake Wick", release_date: "2021"},
+        {id: 3, title: "Joe Wick", release_date: "2022"}
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+    };
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for movies..." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}></input>
+                <button type="submit" className="search-btn">Search</button>
+            </form>
+            <div className="movie-grid">
+                {movies.map((movie) => (<MovieCard movie={movie} key={movie.id} />))}
+            </div>
+        </div>
+    );  
+}
+
+export default Home
